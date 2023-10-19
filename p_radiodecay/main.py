af6b982c2dd7a400d5ebc611aed4d5af5b187de3
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-19 17:41:55 +0300

    Create main.py
    test of radioactivedecay package

---
+++ p_radiodecay/main.py
@@ -0,0 +1,13 @@
+pip install radioactive_decay
+
+from radioactive_decay import RadioactiveDecay
+
+problem = RadioactiveDecay(
+    substance='Uranium-238',
+    initial_amount=1000,
+    half_life=4.5e9,
+    time_steps=100,
+    time_unit='years'
+)
+
+problem.generate_code()
