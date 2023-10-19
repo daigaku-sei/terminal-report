2620e3e249119d58a6b05e50403c7654315cb149
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-19 18:05:39 +0300

    Update main.py

--- p_radiodecay/main.py
+++ p_radiodecay/main.py
@@ -2,20 +2,20 @@ import radioactivedecay as rd
 
 print('\nWelcome to radioactive decay package demo!')
 
-print('\nMo99, of 2.0 Bq, after 20.0 hours, get Bq_activities/cumulative')
+print('\nMo99, of 2.0 Bq, after 20.0 hours,\nget Bq_activities/cumulative')
 Mo99_t0 = rd.Inventory({'Mo-99': 2.0}, 'Bq')
 Mo99_t1 = Mo99_t0.decay(20.0, 'h')
 print(Mo99_t1.activities('Bq'))
 print(Mo99_t0.cumulative_decays(20.0, 'h'))
 
 
-print('\nH-3: 3.0 g, decay for 12.32 years, get masses/fract')
+print('\nH-3: 3.0 g, decay for 12.32 years,\nget masses/fract')
 H3_t0 = rd.Inventory({'H-3': 3.0}, 'g')
 H3_t1 = H3_t0.decay(12.32, 'y')
 print(H3_t1.masses('g'))
 print(H3_t1.mass_fractions())
 
-print('\nC14 3.2E24 num, 3000 years, get moles/fractions')
+print('\nC14: 3.2E24 num, for 3000 years,\nget moles/fractions')
 C14_t0 = rd.Inventory({'C-14': 3.2E24}, 'num')
 C14_t1 = C14_t0.decay(3000, 'y')
 print(C14_t1.moles('mol'))
