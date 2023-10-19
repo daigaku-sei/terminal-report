297b4fb8e21ba04813637d5e2e59e8c6c464cb4b
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-19 17:58:02 +0300

    Update main.py

--- p_radiodecay/main.py
+++ p_radiodecay/main.py
@@ -1,21 +1,23 @@
 import radioactivedecay as rd
 
+print('Mo99, of 2.0 Bq, after 20.0 hours')
 Mo99_t0 = rd.Inventory({'Mo-99': 2.0}, 'Bq')
 Mo99_t1 = Mo99_t0.decay(20.0, 'h')
->>> Mo99_t1.activities('Bq')
+print(Mo99_t1.activities('Bq'))
+print(Mo99_t0.cumulative_decays(20.0, 'h'))
 
-Mo99_t0.cumulative_decays(20.0, 'h')
 
+print('H-3: 3.0 g, decay for 12.32 years')
 H3_t0 = rd.Inventory({'H-3': 3.0}, 'g')
 H3_t1 = H3_t0.decay(12.32, 'y')
-H3_t1.masses('g')
+print(H3_t1.masses('g'))
+print(H3_t1.mass_fractions())
 
-H3_t1.mass_fractions()
-
+print('C14 3.2E24 num, 3000 years')
 C14_t0 = rd.Inventory({'C-14': 3.2E24}, 'num')
 C14_t1 = C14_t0.decay(3000, 'y')
-C14_t1.moles('mol')
-C14_t1.mole_fractions()
+print(C14_t1.moles('mol'))
+print(C14_t1.mole_fractions())
 
 Mo99_t0.plot(20, 'd', yunits='Bq')
 
