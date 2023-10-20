5d403ec93265018d4e84ec96748ee0e31327df51
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-20 19:34:51 +0300

    Update main.cpp

--- cpp2/main.cpp
+++ cpp2/main.cpp
@@ -2,18 +2,113 @@
 #include <Eigen/Dense>
 
 int main() {
-    Eigen::MatrixXd A(2, 2);
-    Eigen::MatrixXd B(2, 2);
+    int rows1, cols1, rows2, cols2;
 
-    A << 1, 2,
-         3, 4;
+    // Ask user for rows and columns of the first matrix
+    do {
+        std::cout << "Enter the number of rows for the first matrix: ";
+        std::cin >> rows1;
+        std::cout << "Enter the number of columns for the first matrix: ";
+        std::cin >> cols1;
+    } while (rows1 <= 0 || cols1 <= 0);
 
-    B << 5, 6,
-         7, 8;
+    // Ask user for rows and columns of the second matrix
+    do {
+        std::cout << "Enter the number of rows for the second matrix: ";
+        std::cin >> rows2;
+        std::cout << "Enter the number of columns for the second matrix: ";
+        std::cin >> cols2;
+    } while (rows2 <= 0 || cols2 <= 0);
 
-    Eigen::MatrixXd C = A * B;
+    // Create matrixes with random data
+    Eigen::MatrixXd matrix1 = Eigen::MatrixXd::Random(rows1, cols1) * 100.0;
+    Eigen::MatrixXd matrix2 = Eigen::MatrixXd::Random(rows2, cols2) * 100.0;
 
-    std::cout << "Matrix C:\n" << C << std::endl;
+    // Get ranks
+    int rank1 = matrix1.fullPivLu().rank();
+    int rank2 = matrix2.fullPivLu().rank();
+
+    // Transpose
+    Eigen::MatrixXd transpose1 = matrix1.transpose();
+    Eigen::MatrixXd transpose2 = matrix2.transpose();
+
+    // Determinant, trace, and inverse (if possible)
+    double determinant1 = matrix1.determinant();
+    double determinant2 = matrix2.determinant();
+
+    double trace1 = matrix1.trace();
+    double trace2 = matrix2.trace();
+
+    Eigen::MatrixXd inverse1, inverse2;
+    bool invertible1 = false, invertible2 = false;
+
+    if (matrix1.rows() == matrix1.cols()) {
+        inverse1 = matrix1.inverse();
+        invertible1 = true;
+    }
+
+    if (matrix2.rows() == matrix2.cols()) {
+        inverse2 = matrix2.inverse();
+        invertible2 = true;
+    }
+
+    // Check if matrices are equal
+    bool equal = (matrix1 == matrix2);
+
+    // Get sum and product (if possible)
+    Eigen::MatrixXd sum, product;
+    bool addable = (matrix1.rows() == matrix2.rows() && matrix1.cols() == matrix2.cols());
+    bool multipliable = (matrix1.cols() == matrix2.rows());
+
+    if (addable) {
+        sum = matrix1 + matrix2;
+    }
+
+    if (multipliable) {
+        product = matrix1 * matrix2;
+    }
+
+    // Print the results
+    std::cout << "Matrix 1:\n" << matrix1 << std::endl;
+    std::cout << "Matrix 2:\n" << matrix2 << std::endl;
+
+    std::cout << "Rank of Matrix 1: " << rank1 << std::endl;
+    std::cout << "Rank of Matrix 2: " << rank2 << std::endl;
+
+    std::cout << "Transpose of Matrix 1:\n" << transpose1 << std::endl;
+    std::cout << "Transpose of Matrix 2:\n" << transpose2 << std::endl;
+
+    std::cout << "Determinant of Matrix 1: " << determinant1 << std::endl;
+    std::cout << "Determinant of Matrix 2: " << determinant2 << std::endl;
+
+    std::cout << "Trace of Matrix 1: " << trace1 << std::endl;
+    std::cout << "Trace of Matrix 2: " << trace2 << std::endl;
+
+    if (invertible1) {
+        std::cout << "Inverse of Matrix 1:\n" << inverse1 << std::endl;
+    } else {
+        std::cout << "Matrix 1 is not invertible." << std::endl;
+    }
+
+    if (invertible2) {
+        std::cout << "Inverse of Matrix 2:\n" << inverse2 << std::endl;
+    } else {
+        std::cout << "Matrix 2 is not invertible." << std::endl;
+    }
+
+    std::cout << "Are the matrices equal? " << (equal ? "Yes" : "No") << std::endl;
+
+    if (addable) {
+        std::cout << "Sum of Matrix 1 and Matrix 2:\n" << sum << std::endl;
+    } else {
+        std::cout << "Matrices cannot be added." << std::endl;
+    }
+
+    if (multipliable) {
+        std::cout << "Product of Matrix 1 and Matrix 2:\n" << product << std::endl;
+    } else {
+        std::cout << "Matrices cannot be multiplied." << std::endl;
+    }
 
     return 0;
 }
