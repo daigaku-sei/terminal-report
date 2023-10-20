3147a2cff86bf94101738f158ff8b9e98fec8e87
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-20 19:41:02 +0300

    Update main.cpp

--- cpp2/main.cpp
+++ cpp2/main.cpp
@@ -1,3 +1,4 @@
+cpp
 #include <iostream>
 #include <Eigen/Dense>
 
@@ -33,21 +34,21 @@ int main() {
     Eigen::MatrixXd transpose2 = matrix2.transpose();
 
     // Determinant, trace, and inverse (if possible)
-    double determinant1 = matrix1.determinant();
-    double determinant2 = matrix2.determinant();
-
-    double trace1 = matrix1.trace();
-    double trace2 = matrix2.trace();
-
+    double determinant1 = 0.0, determinant2 = 0.0;
+    double trace1 = 0.0, trace2 = 0.0;
     Eigen::MatrixXd inverse1, inverse2;
     bool invertible1 = false, invertible2 = false;
 
     if (matrix1.rows() == matrix1.cols()) {
+        determinant1 = matrix1.determinant();
+        trace1 = matrix1.trace();
         inverse1 = matrix1.inverse();
         invertible1 = true;
     }
 
     if (matrix2.rows() == matrix2.cols()) {
+        determinant2 = matrix2.determinant();
+        trace2 = matrix2.trace();
         inverse2 = matrix2.inverse();
         invertible2 = true;
     }
@@ -78,19 +79,17 @@ int main() {
     std::cout << "Transpose of Matrix 1:\n" << transpose1 << std::endl;
     std::cout << "Transpose of Matrix 2:\n" << transpose2 << std::endl;
 
-    std::cout << "Determinant of Matrix 1: " << determinant1 << std::endl;
-    std::cout << "Determinant of Matrix 2: " << determinant2 << std::endl;
-
-    std::cout << "Trace of Matrix 1: " << trace1 << std::endl;
-    std::cout << "Trace of Matrix 2: " << trace2 << std::endl;
-
     if (invertible1) {
+        std::cout << "Determinant of Matrix 1: " << determinant1 << std::endl;
+        std::cout << "Trace of Matrix 1: " << trace1 << std::endl;
         std::cout << "Inverse of Matrix 1:\n" << inverse1 << std::endl;
     } else {
         std::cout << "Matrix 1 is not invertible." << std::endl;
     }
 
     if (invertible2) {
+        std::cout << "Determinant of Matrix 2: " << determinant2 << std::endl;
+        std::cout << "Trace of Matrix 2: " << trace2 << std::endl;
         std::cout << "Inverse of Matrix 2:\n" << inverse2 << std::endl;
     } else {
         std::cout << "Matrix 2 is not invertible." << std::endl;
