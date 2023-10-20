cbfb276293553fe62c3c7463adbe8113574dbfd3
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-20 21:12:49 +0300

    Update main.py

--- p1/main.py
+++ p1/main.py
@@ -1,61 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 20 20:58:24 2023
+Used black to autoformat
+Used Spyder code analysis
+
+@author: 325A R
+"""
+
+
 import random
+
+
 print("______\n")
 print("\n p1: [9] => T3,I3,M1: array operations on 1x[N] of random int\n")
 print("______")
+
 # Ask user for a number
-number = int(input("Guess a number to be randomed (-100 to 100): "))
+number = int(input("Enter a number: "))
 
 # Create an array of random integers from -100 to 100
 array = [random.randint(-100, 100) for _ in range(10)]
 
 # Initialize variables
-sum = 0
-product = 1
-min_value = float('inf')
+SUM = 0
+PRODUCT = 1
+min_value = float("inf")
 min_indexes = []
-max_value = float('-inf')
+max_value = float("-inf")
 max_indexes = []
 guess_indexes = []
-guess_count = 0
+GUESS_COUNT = 0
 
 # Collect data while initializing
 for i, num in enumerate(array):
-    sum += num
-    product *= num
-    
+    SUM += num
+    PRODUCT *= num
+
     if num < min_value:
         min_value = num
         min_indexes = [i]
     elif num == min_value:
         min_indexes.append(i)
-    
+
     if num > max_value:
         max_value = num
         max_indexes = [i]
     elif num == max_value:
         max_indexes.append(i)
-    
+
     if num == number:
         guess_indexes.append(i)
-        guess_count += 1
+        GUESS_COUNT += 1
 
 # Print the array
 print("Array:", array)
 
 # Print the properties
-print("Sum:", sum)
-print("Product:", product)
-print("Minimum Value:", min_value)
+print("Sum:", SUM)
+print("Product:", PRODUCT)
+print("\nMinimum Value:", min_value)
 print("Minimum Indexes:", min_indexes)
 print("Maximum Value:", max_value)
 print("Maximum Indexes:", max_indexes)
-print("Number", number, "Presence:")
+print("\nNumber", number, "Presence:")
 print("Indexes:", guess_indexes)
-print("Count:", guess_count)
+print("Count:", GUESS_COUNT)
 
 # Calculate mean and geometric mean
-mean = sum / len(array)
-geometric_mean = product ** (1 / len(array))
+mean = SUM / len(array)
+geometric_mean = PRODUCT ** (1 / len(array))
 
 # Print mean and geometric mean
 print("Mean:", mean)
@@ -71,7 +84,8 @@ for i in range(1, len(array)):
     array[j + 1] = key
 
 # Print the sorted array in ascending order
-print("Sorted Array (Ascending):", array)
+print("\nSorted Array (Ascending):", array)
 
 # Print the sorted array in descending order
 print("Sorted Array (Descending):", array[::-1])
+print("\n")
