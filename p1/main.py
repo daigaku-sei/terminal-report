25c09c3c5260c11e18ac41c9517cad46fcd21ef7
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-21 16:27:32 +0300

    Update main.py

--- p1/main.py
+++ p1/main.py
@@ -175,6 +175,6 @@ print("Sorted Array (Descending):", sorted_array[::-1])
 # Search for a value in the array until the user types 666
 print(
     "Beginning the search for number feature...\n
-    You may now search for any number if it present or not in a array"
+    \You may now search for any number if it present or not in a array"
 )
 search_value(array)
