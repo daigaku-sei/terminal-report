ddb0c603993fff03c07e4683bc465a95dcaa8011
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-31 19:30:34 +0300

    Update test.py

--- p_radiodecay/test.py
+++ p_radiodecay/test.py
@@ -1,9 +1,11 @@
+import matplotlib.pyplot as plt
 import radioactivedecay as rd
-import termux
 
-# Create the plot
+# Create a nucleid object
 nuc = rd.nucleid('Mo-99')
-plot = nuc.plot(kind='line', x='energy', y='intensity')
 
-# Display the plot in a new ion window
-termux.ion(plot)
+# Plot the decay chain
+nuc.plot()
+
+# Display the plot inline
+plt.show()
