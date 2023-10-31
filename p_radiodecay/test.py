79382d30bf01a4c2c18e318b8ddef34518aaf752
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-31 19:32:44 +0300

    Update test.py

--- p_radiodecay/test.py
+++ p_radiodecay/test.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import radioactivedecay as rd
 
 # Create a nucleid object
-nuc = rd.nucleid('Mo-99')
+nuc = rd.Nuclide('Mo-99')
 
 # Plot the decay chain
 nuc.plot()
